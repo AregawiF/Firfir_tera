7b113afe20c935baf5c671e57c2b2feba82b1e28
AregawiF <133565354+AregawiF@users.noreply.github.com>
2024-11-04 14:52:17 +0300

    edited url

--- client/src/services/authApi.ts
+++ client/src/services/authApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'http://localhost:3000', 
+    baseUrl: 'https://firfir-tera-app-api.onrender.com', 
   }),
   endpoints: (builder) => ({
     signup: builder.mutation({
--- client/src/services/usersApi.ts
+++ client/src/services/usersApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://firfir-tera-app-api.onrender.com/',
   prepareHeaders: (headers, { getState }) => {
     const token = localStorage.getItem('token'); 
     if (token) {
