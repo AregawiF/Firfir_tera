a561089a18a9abbd961458446873bbc8e8172909
AregawiF <133565354+AregawiF@users.noreply.github.com>
2024-11-04 09:55:05 +0300

    edit vercel issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,23 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 import * as path from 'path';
 import { mkdirSync } from 'fs';
 import serverlessExpress from '@vendia/serverless-express';
-import { Handler, Context, Callback, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const uploadsDir = path.join(__dirname, '..', 'server', 'tmp', 'uploads');
   mkdirSync(uploadsDir, { recursive: true }); 
-
-//   app.enableCors(
-//     {
-//   origin: '*',
-//   // methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//   credentials: true,
-//   allowedHeaders: 'Content-Type, Authorization',
-
-// }
-// )
   app.useGlobalPipes(new ValidationPipe());
   app.use(express.json({ limit: '10mb' }))
   // await app.listen(3000);
